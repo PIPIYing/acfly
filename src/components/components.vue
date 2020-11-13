d1e6ab2a26905d573f8f978374dd970ac935fd95
PIPIYing <19924338329@163.com>
2020-11-14 01:08:54 +0800

    acfly项目初始架构

---
+++ src/components/components.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="components">
+    components
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Components",
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="index">
+    index
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Index",
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
